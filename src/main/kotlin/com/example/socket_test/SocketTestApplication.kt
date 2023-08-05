453ab50062ac812ac75ff8534e0485ca96c275f5
= <=>
2023-08-05 20:33:11 +0900

    ⚙️ :: 프로젝트 생성

---
+++ src/main/kotlin/com/example/socket_test/SocketTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.socket_test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocketTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<SocketTestApplication>(*args)
+}
